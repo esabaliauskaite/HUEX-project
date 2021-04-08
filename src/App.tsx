7c45075d58b28c442538f7c416d3adedb652081d
Egle Sabaliauskaite <esabaliauskaite6@gmail.com>
2021-04-09 00:49:14 +0200

    Card Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import './App.css';
+import "./App.css";
+import { Cards } from "./Components/Cards";
 
 function App() {
   return (
     <div className="App">
-      <div className="SidePanel">
+      <div className="SidePanel"></div>
+      <div>
+        <div className="ForecastMenu">
+          <span>Today</span>&nbsp; &nbsp;&nbsp;&nbsp;
+          <span>Week</span>
+        </div>
+        <div className="Title">
+          <p>Today's Highlights</p>
+        </div>
+        <div className="ConditionCards">
+          <Cards></Cards>
+          <Cards></Cards>
+          <Cards></Cards>
+          <Cards></Cards>
+          <Cards></Cards>
+          <Cards></Cards>
+        </div>
       </div>
     </div>
   );
