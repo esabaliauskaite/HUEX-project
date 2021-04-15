15e9c75e55adc9a7bd20aa911b75c1dc2cc11069
Egle Sabaliauskaite <59394960+esabaliauskaite@users.noreply.github.com>
2021-04-15 21:05:20 +0200

    Photo API calls

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -6,8 +6,19 @@ export const fetchWeatherData = (city: string) => {
   return axios.get(url, { headers: { "Content-Type": "application/json" } });
 };
 
-export const fetchPhoto = (city: string) => {
-  let url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?=input=${city}&key=${process.env.REACT_APP_API_PHOTOS_KEY}&inputtype=textquery&fields=name,photos`;
+export const fetchPhotoRef = (city: string) => {
+  let url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${city}&key=AIzaSyDRvZOa-oOOuIb99KHq7bqG2U9f1mpi2m0&inputtype=textquery&fields=name,photos`;
+  return axios.get(url);
+};
+
+export const fetchPhoto = (ref: string) => {
+  let url = `https://maps.googleapis.com/maps/api/place/photo?photoreference=${ref}&key=${process.env.REACT_APP_API_PHOTOS_KEY}&maxwidth=400&maxheight=400`;
+  return axios.get(url, { headers: { "Content-Type": "application/json" } });
+};
+
+export const fetchCurrentLocation = (latitude: any, longtitude: any) => {
+  let url = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longtitude}&key=908276be98cb48d9869155e5f255347b
+  `;
   return axios.get(url, { headers: { "Content-Type": "application/json" } });
 };
 
