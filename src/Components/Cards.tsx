2e3a6a10bb29a107b6f5cf31b7d30f4da361863a
alphaceph91 <56506266+alphaceph91@users.noreply.github.com>
2021-04-16 17:13:15 +0200

    Update fix

--- src/Components/Cards.tsx
+++ src/Components/Cards.tsx
@@ -1,8 +1,33 @@
+import { ReactNode } from "react";
 
+type CardProps = {
+  title: string;
+  value?: string;
+  value2?: string;
+  value3?: string;
+  value4?: string;
+  src?: string;
+  children?: ReactNode;
+};
 
-export const Cards = () => {
-    return (
-        <div className="Card">  
-        </div>
-    )
-}
+export const Cards = ({
+  title,
+  value,
+  value2,
+  value3,
+  value4,
+  src,
+  children,
+}: CardProps) => {
+  return (
+    <div className="Card">
+      <p className="CardTitle">{title}</p>
+      <img src={src} className="OneImg" alt={title} id={title}></img>
+      <p className="CardValue">{value}</p>
+      <p className="Sun">{value2}</p>
+      <p className="Sun">{value3}</p>
+      <p className="Dir">{value4}</p>
+      {children}
+    </div>
+  );
+};
